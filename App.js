0c56922533fd7539d5304b15aba33e2b9eed80d8
AngelQ87 <angelquintero87@gmail.com>
2019-03-24 16:00:03 -0500

    listo diseno de presentacion

--- App.js
+++ App.js
@@ -5,11 +5,10 @@ import { StyleSheet,
          Button, 
          Alert, 
          ImageBackground,
-         Image } from 'react-native';
+         Image,
+        } from 'react-native';
  
-import imagenfondo from './component/images/fondo.png' ;   
 
-const remote = 'https://github.com/AngelQ87/aquinsoftweb/blob/master/src/images/15.jpg';
 
 export default class App extends React.Component {
   
@@ -26,40 +25,49 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground 
-         source={{ uri: 'https://github.com/AngelQ87/aquinsoftweb/blob/master/src/images/15.jpg'}}
-         style={{width: '100%', height: '100%', flex: 1, }}
-         imageStyle={{resizeMode: 'stretch'}}  >
-          <View style={{ width: 'auto', height: 30, backgroundColor: 'powderblue' }}>
-          </View>
-          <View>
-            <Text style={styles.red}>Prueba App</Text>
-            <Button title='Inicio' onPress={this.button_inicio} color="#841584" />
-          </View>
-          <View style={{ width: 'auto', height: 30 }}>
-            <Text>Powered by Ing. Angel Quintero</Text>
-          </View>
-        </ImageBackground>
-      </View>
+      
+      <ImageBackground style={styles.container} source={require('./component/images/fondo.png')}
+        imageStyle={{ resizeMode: 'stretch' }} >
+        <View style={styles.inner}>
+           <View style={{ width: 'auto', height: 30 }}></View>
+           <View style={{width: 'auto' , height: '50%' }}>
+              <View>
+                <Image style={{width: 300 , height: 120}} source={require('./component/images/logo.png')}></Image>
+              </View>
+              <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{ width: 'auto', height: 30 }}></View>
+                <View style={{ width: 200, paddingTop: 50}}>
+                  <Button title='Crear cuenta' onPress={this.button_inicio} />
+                </View>
+                <View style={{ width: 200, paddingTop: 30}}>
+                  <Button title='Iniciar sesion' onPress={this.button_inicio} color="#841584" />
+                </View>
+              </View> 
+           </View>
+           <View style={{ width: 'auto', height: 30 }}>
+              <Text>Powered by Ing. Angel Quintero</Text>
+           </View>
+        </View>        
+      </ImageBackground>
+     
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'skyblue',    
-    alignItems: 'center',
-    //justifyContent: 'center',    
-    padding: 30,  
-    flexDirection: 'column',
-    justifyContent: 'space-between',     
+    flex: 1,               
   },
-  red: {
-    color: '#841584',
-    fontSize: 30, 
-    padding: 20,
+  inner: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255, .6)',
+    alignItems: 'center',
+    justifyContent: 'center', 
+    flexDirection: 'column',
+    justifyContent: 'space-between', 
   },
   
+  
+
 });
